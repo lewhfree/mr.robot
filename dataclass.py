55c2a5973ac8208150bf4a38b257e43651ab7e02
lewhfree <lewhfree>
2025-05-21 17:44:41 -0600

    added dataclass

---
+++ dataclass.py
@@ -0,0 +1,19 @@
+class Data:
+    def __init__(self, prompt: str, res: str, children: list = None):
+        self.prompt = prompt
+        self.res = res
+        self.children = children if children is not None else []
+
+    def __str__(self):
+        "class stores prompt, res, children"
+
+    def toDict(self):
+        """
+            This should help facilitate long term storage because you could store the tree as a json file probably
+        """
+        
+        return {
+            "prompt": self.prompt,
+            "result": self.res,
+            "children": [child.toDict() for child in self.children]
+        }
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-# lines that start with a #hash are comments
+# lines that start with a # are comments
 
 # These are imported modules. They add functionality beyond what vanilla python can do
 
-from ollama import chat,ChatResponse # This module helps simplify ollama api access
+from ollama import * # This module helps simplify ollama api access
+                     # that command lets this file access all the functions that the ollama module has
 
+                     
 # This basically asks the chatbot "prompt" and prints it out. This is just placeholder, but it should work
 response: ChatResponse = chat(
     model="codellama:latest",
